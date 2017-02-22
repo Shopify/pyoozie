6d442dc3e99a46e92d700e81d89d9cc56126406f
Chris Fournier <chris.fournier@shopify.com>
2017-02-22 15:11:39 -0500

    Remove coordinator from pyoozie.xml

--- pyoozie/xml/__init__.py
+++ pyoozie/xml/__init__.py
@@ -2,9 +2,9 @@
 # Use of this source code is governed by a MIT-style license that can be found in the LICENSE file.
 from __future__ import unicode_literals
 
-from pyoozie.xml._coordinator import Coordinator, format_datetime
+from pyoozie.xml._coordinator import format_datetime
 
 __all__ = (
     # coordinator
-    'Coordinator', 'format_datetime',
+    'format_datetime',
 )
