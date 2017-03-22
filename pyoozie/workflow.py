19df089642c4b316d41dff86f51624049dbcc7d2
Chris Fournier <chris.fournier@shopify.com>
2017-03-22 16:09:26 -0400

    Fix type annotations

--- pyoozie/workflow.py
+++ pyoozie/workflow.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a MIT-style license that can be found in the LICENSE file.
 import typing  # pylint: disable=unused-import
 
+from pyoozie import tags  # pylint: disable=unused-import
+
 
 class AbstractAction(object):
     pass
@@ -11,7 +13,7 @@ class Action(AbstractAction):
     """Concrete action to execute (takes an action tag, e.g. pyoozie.Shell, pyoozie.Email. etc.)"""
 
     def __init__(self, action_tag, on_error=None):
-        # type: (tags.ActionTag, AbstractAction) -> None
+        # type: (tags.Shell, AbstractAction) -> None
         self.__action_tag = action_tag
         self.__on_error = on_error
 
@@ -19,21 +21,19 @@ class Action(AbstractAction):
 class Sequence(AbstractAction):
     """Set of actions to execute sequentially (implemented by chaining actions and 'OK' transitions)"""
 
-    def __init__(self, *actions, on_error=None):
-        # TODO type: (typing.Sequence[AbstractAction], AbstractAction) -> None
-        self.__on_error = on_error
+    def __init__(self, *actions, **kwargs):
+        # type: (typing.Tuple[AbstractAction, ...], typing.Dict[str, AbstractAction]) -> None
+        self.__on_error = kwargs.get('on_error')
         self.__actions = actions
 
 
 class Parallelization(AbstractAction):
     """Set of actions to execute in parallel (implemented as fork/join tag pair)"""
 
-    def __init__(self, *actions, on_error=None):
-        # TODO type: (typing.Iterable[AbstractAction], AbstractAction) -> None
-        self.__on_error = on_error
-        self.__actions = set()
-        for action in actions:
-            self.__actions.add(action)
+    def __init__(self, *actions, **kwargs):
+        # type: (typing.Tuple[AbstractAction, ...], typing.Dict[str, AbstractAction]) -> None
+        self.__on_error = kwargs.get('on_error')
+        self.__actions = set(actions)
 
 
 class Kill(AbstractAction):
