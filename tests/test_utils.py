eb1f38e99903c6b5e1e8d9ba30d32c3193740b58
Chris Fournier <chris.fournier@shopify.com>
2017-03-24 12:43:27 -0400

    Add test utilities

---
+++ tests/test_utils.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 "Shopify inc." All rights reserved.
+# Use of this source code is governed by a MIT-style license that can be found in the LICENSE file.
+from __future__ import unicode_literals
+
+import xml.etree.ElementTree as et
+
+import pytest
+import tests.utils
+
+
+@pytest.fixture
+def valid_workflow():
+    return """
+<workflow-app xmlns="uri:oozie:workflow:0.5" name="descriptive-name">
+    <parameters>
+        <property>
+            <name>property_key</name>
+            <value>property_value</value>
+        </property>
+        <property>
+            <name>other_property_key</name>
+            <value>🤣</value>
+        </property>
+    </parameters>
+    <global>
+        <job-tracker>job-tracker</job-tracker>
+        <name-node>name-node</name-node>
+    </global>
+    <start to="end" />
+    <end name="end" />
+</workflow-app>
+""".strip()
+
+
+def test_xml_to_dict_unordered():
+    document_dict = tests.utils.xml_to_dict_unordered("""
+<root>
+  <tag />
+  <tag key="value" />
+  <tag>Text</tag>
+</root>
+    """.strip())
+    assert document_dict == {
+        'root': {
+            'tag': [None, u'Text', {'@key': 'value'}]
+        }
+    }
+
+
+def test_assert_valid_workflow(valid_workflow):
+    # A minimal workflow should validate
+    tests.utils.assert_valid_workflow(valid_workflow)
+
+    # With valid XML, duplicate names should raise an assertion error
+    with pytest.raises(AssertionError) as assertion_info:
+        xml = """
+<workflow-app xmlns="uri:oozie:workflow:0.5" name="descriptive-name">
+    <action name="alpha"/>
+    <action name="alpha"/>
+    <action name="beta"/>
+</workflow-app>""".strip()
+        assert len(et.fromstring(xml)), 'Invalid test XML'
+        tests.utils.assert_valid_workflow(xml)
+    assert str(assertion_info.value) == 'Name(s) reused: alpha'
+
+    # With valid XML, a missing a start tag should result in a schema violation assertion error
+    with pytest.raises(AssertionError) as assertion_info:
+        xml = """
+<workflow-app xmlns="uri:oozie:workflow:0.5" name="descriptive-name">
+    <global>
+        <job-tracker>job-tracker</job-tracker>
+        <name-node>name-node</name-node>
+    </global>
+    <end name="end" />
+</workflow-app>""".strip()
+        assert len(et.fromstring(xml)), 'Invalid test XML'
+        tests.utils.assert_valid_workflow(xml)
+    assert str(assertion_info.value).strip() == str(
+        '''An XML validation error occurred.
+
+Error: Invalid app definition, org.xml.sax.SAXParseException; lineNumber: 6; columnNumber: 23; '''
+        '''cvc-complex-type.2.4.a: Invalid content was found starting with element 'end'. One of '''
+        ''''{"uri:oozie:workflow:0.5":credentials, "uri:oozie:workflow:0.5":start}' is expected.
+
+Parsing:
+
+''' + xml).strip()
+
+    # With invalid XML, a parsing error should occur
+    with pytest.raises(et.ParseError) as assertion_info:
+        xml = '<workflow-app xmlns="uri:oozie:workflow:0.5" name="descriptive-name">'
+        tests.utils.assert_valid_workflow(xml)
+    assert str(assertion_info.value) == 'no element found: line 1, column 38'
+
+
+def test_parsed_xml_assert_node(valid_workflow):
+    app = tests.utils.ParsedXml(valid_workflow)
+
+    # Asserting that a node that exists (once) should pass
+    app.assert_node('/global')
+
+    # Asserting that a node that doesn't exists should raise an assertion error
+    with pytest.raises(AssertionError) as assertion_info:
+        app.assert_node('/not_a_tag')
+    assert str(assertion_info.value) == str("Could not find xml tag at xpath '/not_a_tag'")
+
+    # Asserting that a node that exists (when it does multiple times) should raise an assertion error
+    with pytest.raises(AssertionError) as assertion_info:
+        app.assert_node('/parameters/property')
+    assert str(assertion_info.value) == str("Found more than one resolution of xpath '/parameters/property'")
+
+    # Asserting that a node has a specific attribute should pass
+    app.assert_node('/start', to='end')
+
+    # Asserting that a node has a specific attribute value that it doens't should raise an assertion error
+    with pytest.raises(AssertionError) as assertion_info:
+        app.assert_node('/start', to=str('not_end'))
+    assert str(assertion_info.value) == str("{'to': 'not_end'} != {'to': 'end'}")
+
+    # Asserting that a node has a specific attribute that it's missing should raise an assertion error
+    with pytest.raises(AssertionError) as assertion_info:
+        app.assert_node('/start', not_a_key=str('end'))
+    assert str(assertion_info.value) == str("{'not_a_key': 'end'} != {'to': 'end'}")
--- tests/utils.py
+++ tests/utils.py
@@ -2,9 +2,13 @@
 # Use of this source code is governed by a MIT-style license that can be found in the LICENSE file.
 from __future__ import unicode_literals
 
+import collections
+import re
 import subprocess
 import tempfile
+import xml.etree.ElementTree as et
 
+import six
 import xmltodict
 
 
@@ -19,7 +23,45 @@ def xml_to_dict_unordered(xml):
     return unorder(xmltodict.parse(xml))
 
 
+NAMESPACE_ATTRIBUTE = re.compile(' xmlns="[^"]+"', flags=re.UNICODE)
+
+
+class ParsedXml(object):
+
+    def __init__(self, xml_string):
+        if isinstance(xml_string, six.binary_type):
+            xml_string = xml_string.decode('utf-8')
+        xml_string = NAMESPACE_ATTRIBUTE.sub(str(''), xml_string.strip())
+        self.tree = et.ElementTree(et.fromstring(xml_string.encode('utf-8')))
+
+    def __get_elements(self, xpath):
+        elements = self.tree.findall(xpath)
+        assert elements, "Could not find xml tag at xpath '%s'" % xpath
+        return elements
+
+    def __get_element(self, xpath):
+        elements = self.__get_elements(xpath)
+        assert len(elements) == 1, \
+            "Found more than one resolution of xpath '%s'" % xpath
+        return elements[0]
+
+    def assert_node(self, xpath, **kwargs):
+        element = self.__get_element(xpath)
+        if kwargs:
+            assert set(kwargs.items()) <= set(element.attrib.items()), '%r != %r' % (kwargs, element.attrib)
+
+
 def assert_valid_workflow(xml):
+    # Check for duplicate names (valid XML and valid schema, but logically invalid)
+    names = ParsedXml(xml).tree.findall('.//*[@name]')
+    names = [name.attrib['name'] for name in names]
+    duplicate_names = [item for item, count in collections.Counter(names).items() if count > 1]
+    assert not duplicate_names, 'Name(s) reused: %s' % ', '.join(sorted(duplicate_names))
+
+    if isinstance(xml, six.text_type):
+        xml = xml.encode('utf-8')
+
+    # Call Oozie to validate XML against the schema
     with tempfile.NamedTemporaryFile() as fp:
         # Write XML file
         fp.write(xml)
@@ -34,7 +76,7 @@ def assert_valid_workflow(xml):
                 shell=True
             )
         except subprocess.CalledProcessError as e:
-            raise AssertionError('An XML validation error\n\n{error}\n\noccurred while parsing:\n\n{xml}'.format(
+            raise AssertionError('An XML validation error occurred.\n\n{error}\n\nParsing:\n\n{xml}'.format(
                 error=e.output.decode('utf8').strip(),
-                xml=xml,
+                xml=xml.decode('utf-8'),
             ))
