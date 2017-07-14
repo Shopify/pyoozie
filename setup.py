2d62cae2949024dcff8555c92cc77ab8ce39eb01
Jason White <jason.white@shopify.com>
2017-07-14 11:52:26 -0400

    remote mypy

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setuplib.setup(
             'autopep8',
             'flake8',
             'mock',
-            'mypy;python_version>="3.3"',
             'pycodestyle == 2.2.0',
             'pylint>=1.6.5,<1.7',
             'pytest-cov',
